3afa98f4a6e598be353cbe3424ae05dfd2bc8df5
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-08-09 17:25:54 +0200

    test.js edited, quatation marks added

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 var myAge = 26;
 
-if (myAge < 18 || myAge >30 ||myAge === 26){
-    document.write ("you aint coming!")} 
-else {document.write (You can come, you cool dude!)}
+if (myAge < 18 || myAge > 30 || myAge === 26) {
+  document.write("you aint coming!");
+} else {
+  document.write("You can come, you cool dude!");
+}
