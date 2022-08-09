65227e676f745fe0f4732f31ed23b456fd8ee082
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-08-09 17:24:28 +0200

    lesson about logical operators

--- test.js
+++ test.js
@@ -1,12 +1,5 @@
-var myAge = 38;
-if (myAge > 40) {
-  document.write("you are over 40!");
-} else if (myAge > 30) {
-  document.write("you are over 30!");
-} else if (myAge > 20) {
-  document.write("you are over 20!");
-} else if (myAge > 10) {
-  document.write("you are over 10!");
-} else {
-  document.write("you are not over 10!");
-}
+var myAge = 26;
+
+if (myAge < 18 || myAge >30 ||myAge === 26){
+    document.write ("you aint coming!")} 
+else {document.write (You can come, you cool dude!)}
