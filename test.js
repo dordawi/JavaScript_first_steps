b92b6a5b1e6091bf32ac6f8252f118a73ca60a53
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-08-10 13:54:54 +0200

    lesson 28 _creating a new Java object

--- test.js
+++ test.js
@@ -1,5 +1,23 @@
-var myArray = [0, 1, 2, 3, "hello"];
-console.log(myArray);
-myArray[4] = "hi";
-myArray.length;
-myArray.sort;
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hello";
+
+var myCar = new Object();
+myCar.maxspeed = 50;
+myCar.driver = "Dorota";
+myCar.drive = function () {
+  console.log("now driving");
+};
+
+myCar.drive();
+
+var myCar2 = {
+  maxSpeed: 70,
+  driver: "Kacpi",
+  drive: function (speed, time) {
+    console.log(speed * time);
+  },
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
