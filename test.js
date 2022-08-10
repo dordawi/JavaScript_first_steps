d6d784ce54bcd3eb4f48c5968abf93c32ea934ea
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-08-10 13:05:30 +0200

    lesson 25 aboput slice and split strings

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
-var myString = 'I\'am a "fun" ninja string';
-
-if (myString.indexOf("ninja") === -1) {
-  console.log("the word ninja is not in the string");
-} else {
-  console.log(
-    "the word ninja starts at the position" + myString.indexOf("ninja")
-  );
-}
-console.log(myString.length);
-console.log(myString.toUpperCase());
+var str = "hello, world";
+var str2 = str.slice(2, 9);
+var str3 = str.slice(2);
+var tags = "meta, ham, salami, pork";
+var tagArray = tags.split(",");
