36cdf618616dcfece7bde04a8d22c76c4166c3d6
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-08-10 14:03:26 +0200

    lesson 29_this

--- test.js
+++ test.js
@@ -1,23 +1,14 @@
-var myArray = new Array();
-myArray[0] = 8;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.maxspeed = 50;
-myCar.driver = "Dorota";
-myCar.drive = function () {
-  console.log("now driving");
-};
-
-myCar.drive();
-
 var myCar2 = {
   maxSpeed: 70,
   driver: "Kacpi",
   drive: function (speed, time) {
     console.log(speed * time);
   },
+  logDriver: function () {
+    console.log("driver name is " + this.driver);
+  },
 };
 
+myCar2.logDriver();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
