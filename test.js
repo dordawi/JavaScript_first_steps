6f5fff05765bf63e8217361c25ef01539311c1e3
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-08-10 14:37:35 +0200

    lesson 31_the date object

--- test.js
+++ test.js
@@ -1,17 +1,26 @@
-var Car = function (maxSpeed, driver) {
-  this.maxSpeed = maxSpeed;
-  this.driver = driver;
-  this.drive = function (speed, time) {
-    console.log(speed * time);
-  };
-  this.logDriver = function () {
-    console.log("driver name is " + this.driver);
-  };
-};
+var birthday = new Date(1984, 9, 31, 15, 23, 56);
+var birthday2 = new Date(1984, 9, 31, 15, 23, 56);
 
-var myCar = new Car(70, "Ninja");
-var myCar2 = new Car(40, "Jon");
-var myCar3 = new Car(50, "Ann");
+//get month of the date
+console.log(birthday.getMonth());
 
-myCar.drive(50, 3);
-myCar3.logDriver();
+//get the full year
+console.log(birthday.getFullYear());
+
+//get the day of the month
+console.log(birthday.getDate());
+
+//get the day of the week
+console.log(birthday.getDay());
+
+//get the hour of the date
+console.log(birthday.getHours());
+
+//get the number of milliseconds since 1st Jan 1970
+console.log(birthday.getTime());
+
+if (birthday.getTime() == birthday2.getTime()) {
+  console.log("birthdays are equal");
+} else {
+  console.log("birthdays are not equal");
+}
