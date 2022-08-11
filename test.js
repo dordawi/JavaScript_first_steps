e0533833ac329d11e3612cf1b65f88116d5d11e9
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-08-11 08:16:04 +0200

    test,js file added, both index.jtml and style.css file edited, onclick event works

--- test.js
+++ test.js
@@ -1,26 +1,14 @@
-var birthday = new Date(1984, 9, 31, 15, 23, 56);
-var birthday2 = new Date(1984, 9, 31, 15, 23, 56);
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
 
-//get month of the date
-console.log(birthday.getMonth());
-
-//get the full year
-console.log(birthday.getFullYear());
-
-//get the day of the month
-console.log(birthday.getDate());
-
-//get the day of the week
-console.log(birthday.getDay());
-
-//get the hour of the date
-console.log(birthday.getHours());
-
-//get the number of milliseconds since 1st Jan 1970
-console.log(birthday.getTime());
-
-if (birthday.getTime() == birthday2.getTime()) {
-  console.log("birthdays are equal");
-} else {
-  console.log("birthdays are not equal");
-}
+button.onclick = function () {
+  if (content.className == "open") {
+    //shrink the box
+    content.className = "";
+    button.innerHTML = "Show More";
+  } else {
+    //expand the box
+    content.className = "open";
+    button.innerHTML = "Show Less";
+  }
+};
